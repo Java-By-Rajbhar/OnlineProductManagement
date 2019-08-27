293d0e2b6481f3459f430e08ad7ec60915998247
User1 <User1@LP-5CD9176M6P.HCLT.CORP.HCL.IN>
2019-08-27 20:12:44 +0530

    added analysis code

--- src/main/java/com/product/app/service/PurchasePrudectServiceImpl.java
+++ src/main/java/com/product/app/service/PurchasePrudectServiceImpl.java
@@ -2,6 +2,8 @@ package com.product.app.service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +11,13 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.product.app.dto.ProductAnalysisResponseDto;
 import com.product.app.dto.PurchesDetailsDto;
 import com.product.app.entity.Customer;
+import com.product.app.entity.Product;
 import com.product.app.entity.ProductPurchase;
 import com.product.app.repository.CustomerRepository;
+import com.product.app.repository.ProductRepository;
 import com.product.app.repository.PurchaseRepository;
 
 /**
@@ -24,11 +29,14 @@ public class PurchasePrudectServiceImpl implements PurchaseService {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(PurchasePrudectServiceImpl.class);
 
-	@Autowired
+	@Autowired 
 	private PurchaseRepository purchaseRepository;
 
 	@Autowired
 	private CustomerRepository customerRepository;
+	
+	@Autowired
+	ProductRepository productRepository;
 
 	@Override
 	public String purchaseProduct(PurchesDetailsDto purchesDetailsDto) {
@@ -53,6 +61,30 @@ public class PurchasePrudectServiceImpl implements PurchaseService {
 		purchaseRepository.save(productPurchase);
 		
 		return "Purchased successfully.";
+	}
+
+	@Override
+	public ProductAnalysisResponseDto analysis(String dailyDate) {
+		LOGGER.info("Inside analysis method of PurchasePrudectServiceImpl class");
+		List<Integer[]> response = purchaseRepository.getPruductNameAndCount(dailyDate);
+		List<String>  productName = new ArrayList<>();
+		List<Integer>  productCount = new ArrayList<>();
+		LOGGER.info("count data ={}",response.size());
+		
+		response.forEach(data->{
+			int id =  data[0];
+			int count  = (int) data[1];
+			Product product = productRepository.findAllByProductId(id);
+			LOGGER.info("product name ={}",product.getProductName());
+			productName.add(product.getProductName());
+			productCount.add(count);
+		});
+		LOGGER.info("product list={}",productName);
+		LOGGER.info("product count={}",productCount);
+		ProductAnalysisResponseDto dto =  new  ProductAnalysisResponseDto();
+		dto.setProductCount(productCount);
+		dto.setProductName(productName);
+		return dto;
 	}
 
 }
--- src/main/java/com/product/app/service/PurchaseService.java
+++ src/main/java/com/product/app/service/PurchaseService.java
@@ -1,9 +1,12 @@
 package com.product.app.service;
 
+import com.product.app.dto.ProductAnalysisResponseDto;
 import com.product.app.dto.PurchesDetailsDto;
 
 public interface PurchaseService {
 
 	String purchaseProduct(PurchesDetailsDto purchesDetailsDto);
+	
+	public ProductAnalysisResponseDto analysis(String daily);
 
 }
